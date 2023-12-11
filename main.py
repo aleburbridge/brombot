c88b63ee8abdc40d90e0c706c4978d5452d3e080
unknown <aleburbridge@gmail.com>
2023-12-11 18:37:12 -0500

    now checks if theres space

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import interactions
 from interactions import Option, OptionType
 from py1337x import py1337x
-from topsecret import email, password, bot_token
+from topsecret import email, password, bot_token, ost_api_key
 from seedr import SeedrAPI
 
 torrents = py1337x() 
@@ -11,17 +11,24 @@ seedr = SeedrAPI(email=email, password=password)
 search_results = {}
 RESULTS_PER_QUERY = 5
 
-def get_torrent_links_by_title(title, results_per_query):
-    results = torrents.search(title, category='movies', sortBy='seeders', order='desc')
+def get_torrent_links_by_title(search_title):
+    results = torrents.search(search_title, sortBy='seeders', order='desc')
     return results['items'][:RESULTS_PER_QUERY]
 
 def get_magnet_link_from_torrent_link(link):
     return torrents.info(link=link)['magnetLink']
 
-def add_torrent_to_seedr(magnet_link):
-    output = seedr.add_torrent(magnet_link)
-    return output
+def is_space_available(link):
+    torrent_size_GB = torrents.info(link=link)['size']
+    torrent_size_GB = float(torrent_size_GB.split()[0])
+    torrent_size_bytes = int(torrent_size_GB * 1073741824)
 
+    seedr_space_available = seedr.get_drive()['space_max'] - seedr.get_drive()['space_used']
+    
+    return torrent_size_bytes < seedr_space_available
+
+def add_torrent_to_seedr(magnet_link):
+    return seedr.add_torrent(magnet_link)
 
 @bot.command(
     name="pirate",
@@ -36,15 +43,15 @@ def add_torrent_to_seedr(magnet_link):
     ]
 )
 async def pirate_command(ctx: interactions.CommandContext, title: str):
-    top_five = get_torrent_links_by_title(title, RESULTS_PER_QUERY)
-    search_results[ctx.author.id] = top_five
+    top_results = get_torrent_links_by_title(title)
+    search_results[ctx.author.id] = top_results
 
     embed = interactions.Embed(
         title="Top 5 Torrents",
         description=f"Search results for *{title}*",
         color=0xB42F00
     )
-    for index, torrent in enumerate(top_five):
+    for index, torrent in enumerate(top_results):
         embed.add_field(
             name=f"{index + 1}) {torrent['name']}",
             value=f"Size: {torrent['size']} | Seeders: {torrent['seeders']} | Leechers: {torrent['leechers']}",
@@ -57,7 +64,7 @@ async def pirate_command(ctx: interactions.CommandContext, title: str):
             description=torrent['name'][:100],
             value=str(index)
         )
-        for index, torrent in enumerate(top_five)
+        for index, torrent in enumerate(top_results)
     ]
 
     select_menu = interactions.SelectMenu(
@@ -71,10 +78,14 @@ async def pirate_command(ctx: interactions.CommandContext, title: str):
 @bot.component("select_torrent")
 async def on_torrent_select(ctx: interactions.CommandContext, selected_option: interactions.SelectOption):
     index = int(selected_option[0])
-    top_five = search_results[ctx.author.id]
-    torrent_link = top_five[index]['link'] 
+    top_results = search_results[ctx.author.id]
+    torrent_link = top_results[index]['link'] 
     magnet_link = get_magnet_link_from_torrent_link(torrent_link)
-    add_torrent_to_seedr(magnet_link)
-    await ctx.send("Torrent successfully added.")
+
+    if is_space_available(torrent_link):
+        add_torrent_to_seedr(magnet_link)
+        await ctx.send("Torrent successfully added.")
+    else:
+        await ctx.send("Not enough space in seedr for torrent.")
 
 bot.start()
